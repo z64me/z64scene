8a4d3bd21becdc3ac97179e1ef810559da378f7e
z64me <45853441+z64me@users.noreply.github.com>
2024-05-27 14:16:23 -0400

    set up instance render test
    It currently draws the mesh that will be used for actor instances, at the world origin (0, 0, 0). Will later update this to display instance and spawn point positions.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir -p bin
 # new method
 mkdir -p bin/o
 g++ -c src/gui.cpp -Iimgui -Iimgui/backends
-gcc src/*.c z64viewer/src/*.c -c -I z64viewer/include -lm -lglfw -ldl -Wall -Wno-unused-function -Wno-scalar-storage-order
+gcc src/*.c z64viewer/src/*.c -c -I z64viewer/include -I z64viewer/src -lm -lglfw -ldl -Wall -Wno-unused-function -Wno-scalar-storage-order
 mv *.o bin/o
 
 # and link
