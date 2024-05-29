5faca42c76b87ca334166e5941b7457832f2c79f
z64me <45853441+z64me@users.noreply.github.com>
2024-05-29 05:00:35 -0400

    add menu bar and tabbed sidebar
    Still very early. You can resize the sidebar, and there are different pages you can look at by clicking the tabs at the top. You can also toggle its visibility in the 'View' menu.

--- src/gui.cpp
+++ src/gui.cpp
@@ -4,6 +4,311 @@
 #include <stdio.h>
 #define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
+#include <functional>
+
+#if 1 // region: private types
+
+struct GuiSettings
+{
+	bool showSidebar = true;
+};
+
+struct LinkedStringFunc
+{
+	const char *name;
+	void (*func)(void);
+};
+
+#endif
+
+#if 1 // region: private storage
+
+static GuiSettings gGuiSettings;
+
+#endif
+
+#if 1 // region: private functions
+
+// imgui doesn't support this natively, so hack a custom one into it
+static void MultiLineTabBarGeneric(
+	const char *uniqueName
+	, const void *arr[]
+	, const int count
+	, const int grouping
+	, int *which
+	, std::function<const char*(const void*[], int)> getter
+)
+{
+	// here i do some hacky stuff with the ui theme colors so
+	// multiple stacked tab bars appear to the user as a unified
+	// tab bar spanning multiple lines
+	
+	ImGui::PushStyleVar(ImGuiStyleVar_TabBarBorderSize, 0.0f);
+	ImGuiStyle& style = ImGui::GetStyle();
+	ImVec4* colors = style.Colors;
+	ImVec4 oldTabActive = colors[ImGuiCol_TabActive];
+	ImVec4 oldTabHovered = colors[ImGuiCol_TabHovered];
+	ImVec4 oldTab = colors[ImGuiCol_Tab];
+	ImVec4 newTabHovered = ImVec4(0.7, 0, 0, 1.0); // TODO move elsewhere
+	ImVec4 newTabActive = ImVec4(0.5, 0, 0, 1.0);
+	int flags = ImGuiTabBarFlags_NoTooltip | ImGuiTabBarFlags_FittingPolicyResizeDown;
+	
+	colors[ImGuiCol_TabHovered] = newTabHovered;
+	
+	for (int i = 0; i < count; i += grouping)
+	{
+		static char name[1024];
+		
+		sprintf(name, "##%sRow%d", uniqueName, i);
+		
+		// test using buttons
+		/*
+		for (int k = 0; k < grouping && i + k < count; ++k)
+		{
+			int t = i + k;
+			
+			if (k > 0)
+				ImGui::SameLine();
+			
+			ImGui::SmallButton(test[t]);
+		}
+		continue;
+		*/
+		
+		if (i + grouping >= count)
+		{
+			colors[ImGuiCol_TabActive] = oldTab;
+			colors[ImGuiCol_Tab] = oldTab;
+			ImGui::PopStyleVar(ImGuiStyleVar_TabBarBorderSize);
+		}
+		
+		if (ImGui::BeginTabBar(name, flags))
+		{
+			for (int k = 0; k < grouping && i + k < count; ++k)
+			{
+				int t = i + k;
+				
+				if (t == *which)
+				{
+					colors[ImGuiCol_TabActive] = newTabActive;
+					colors[ImGuiCol_Tab] = colors[ImGuiCol_TabActive];
+				}
+				else
+				{
+					colors[ImGuiCol_TabActive] = oldTab;
+					colors[ImGuiCol_Tab] = oldTab;
+				}
+				
+				const char *name = getter(arr, t);
+				
+				if (ImGui::BeginTabItem(name))
+				{
+					if (ImGui::IsItemDeactivated())
+					{
+						//fprintf(stderr, "selected %s\n", name);
+						*which = t;
+					}
+					ImGui::EndTabItem();
+				}
+			}
+			
+			ImGui::EndTabBar();
+		}
+	}
+	
+	colors[ImGuiCol_TabActive] = oldTabActive;
+	colors[ImGuiCol_TabHovered] = oldTabHovered;
+	colors[ImGuiCol_Tab] = oldTab;
+}
+
+static void MultiLineTabBar(const char *uniqueName, char const *arr[], const int count, const int grouping, int *which)
+{
+	MultiLineTabBarGeneric(
+		uniqueName
+		, reinterpret_cast<const void**>(arr)
+		, count
+		, grouping
+		, which
+		, [](const void **arr, const int index) -> const char* {
+			const char **ready = reinterpret_cast<const char**>(arr);
+			return ready[index];
+		}
+	);
+}
+
+static const LinkedStringFunc *gSidebarTabs[] = {
+	new LinkedStringFunc{
+		"General"
+		, [](){
+			ImGui::TextWrapped("TODO: 'General' tab");
+		}
+	},
+	new LinkedStringFunc{
+		"Rooms"
+		, [](){
+			ImGui::TextWrapped("TODO: 'Rooms' tab");
+		}
+	},
+	new LinkedStringFunc{
+		"Scene Env"
+		, [](){
+			ImGui::TextWrapped("TODO: 'Scene Env' tab");
+		}
+	},
+	new LinkedStringFunc{
+		"Room Env"
+		, [](){
+			ImGui::TextWrapped("TODO: 'Room Env' tab");
+		}
+	},
+	new LinkedStringFunc{
+		"Collision & Exits"
+		, [](){
+			ImGui::TextWrapped("TODO: 'Collision & Exits' tab");
+		}
+	},
+	new LinkedStringFunc{
+		"Spawns"
+		, [](){
+			ImGui::TextWrapped("TODO: 'Spawns' tab");
+		}
+	},
+	new LinkedStringFunc{
+		"Pathways"
+		, [](){
+			ImGui::TextWrapped("TODO: 'Pathways' tab");
+		}
+	},
+	new LinkedStringFunc{
+		"Instances"
+		, [](){
+			ImGui::TextWrapped("TODO: 'Instances' tab");
+		}
+	},
+	new LinkedStringFunc{
+		"Objects"
+		, [](){
+			ImGui::TextWrapped("TODO: 'Objects' tab");
+		}
+	},
+};
+
+static void DrawSidebar(void)
+{
+	ImGuiIO& io = ImGui::GetIO();
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoMove
+		| ImGuiWindowFlags_NoSavedSettings
+		| ImGuiWindowFlags_NoFocusOnAppearing
+		| ImGuiWindowFlags_NoNav
+		| ImGuiWindowFlags_NoTitleBar
+		| ImGuiWindowFlags_NoBringToFrontOnFocus
+		| ImGuiWindowFlags_NoMove
+	;
+	
+	// positioning
+	{
+		const ImGuiViewport* viewport = ImGui::GetMainViewport();
+		const int minWidth = 256;
+		int maxWidth = -1;
+		
+		ImVec2 work_pos = viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
+		ImVec2 work_size = viewport->WorkSize;
+		ImVec2 window_pos, window_pos_pivot;
+		
+		maxWidth = work_size.x / 2;
+		if (maxWidth < minWidth)
+			maxWidth = minWidth;
+		
+		window_pos.x = work_pos.x + work_size.x;
+		window_pos.y = work_pos.y;
+		window_pos_pivot.x = 1;
+		window_pos_pivot.y = 0;
+		
+		ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+		ImGui::SetNextWindowSizeConstraints(ImVec2(minWidth, work_size.y), ImVec2(maxWidth, work_size.y));
+		ImGui::SetNextWindowBgAlpha(0.35f); // translucent background
+	}
+	
+	if (ImGui::Begin("Sidebar", 0, window_flags))
+	{
+		static int which = 0;
+		
+		MultiLineTabBarGeneric(
+			"SidebarTabs"
+			, reinterpret_cast<const void**>(gSidebarTabs)
+			, sizeof(gSidebarTabs) / sizeof(*gSidebarTabs)
+			, 5
+			, &which
+			, [](const void **arr, const int index) -> const char* {
+				const LinkedStringFunc **ready = reinterpret_cast<const LinkedStringFunc**>(arr);
+				return ready[index]->name;
+			}
+		);
+		//ImGui::TextWrapped(gSidebarTabs[which]->name); // test
+		
+		// draw the selected sidebar
+		gSidebarTabs[which]->func();
+		
+		// example using MultiLineTabBar(), for illustrative purposes
+		/*
+		static char const *test[] = {
+			"General", "Rooms", "Scene Env", "Room Env",
+			"Collision & Exits", "Spawns", "Pathways", "Instances", "Objects"
+		};
+		MultiLineTabBar("SidebarTabsExample", test, sizeof(test) / sizeof(*test), 5, &which);
+		ImGui::TextWrapped(test[which]);
+		*/
+		
+		// maybe use vertical tabs w/ icons and tooltips eventually (proof of concept)
+		/*
+		const ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::PushClipRect((ImVec2){0, 0}, viewport->Size, false);
+		for (int i = 0; i < sizeof(test) / sizeof(*test); ++i)
+		{
+			ImGui::SetCursorPos((ImVec2){-64, i * 32.0f});
+			ImGui::Button(test[i]);
+		}
+		ImGui::PopClipRect();
+		*/
+	}
+	ImGui::End();
+}
+
+static void DrawMenuBar(void)
+{
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("New"))
+			{
+			}
+			if (ImGui::MenuItem("Open", "Ctrl+O"))
+			{
+				fprintf(stderr, "wow!\n");
+			}
+			if (ImGui::MenuItem("Save", "Ctrl+S"))
+			{
+			}
+			if (ImGui::MenuItem("Save as..."))
+			{
+			}
+			ImGui::Separator();
+			if (ImGui::MenuItem("Exit", "Ctrl+Q"))
+			{
+			}
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("View"))
+		{
+			if (ImGui::MenuItem("Sidebar", NULL, &gGuiSettings.showSidebar));
+			
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
+}
+#endif // end private functions
 
 extern "C" void GuiInit(GLFWwindow *window)
 {
@@ -51,6 +356,11 @@ extern "C" void GuiDraw(GLFWwindow *window)
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplGlfw_NewFrame();
 	ImGui::NewFrame();
+	
+	DrawMenuBar();
+	
+	if (gGuiSettings.showSidebar)
+		DrawSidebar();
 	
 	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
 	if (show_demo_window)
