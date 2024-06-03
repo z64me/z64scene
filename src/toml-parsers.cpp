cea0f6a6cc5c4de676bee23f30b731c93aa556a3
z64me <45853441+z64me@users.noreply.github.com>
2024-06-03 01:13:36 -0400

    TOML parsing WIP
    Parses a placeholder actor database from an embedded TOML. Still very WIP.

--- src/toml-parsers.cpp
+++ src/toml-parsers.cpp
@@ -7,9 +7,14 @@
 
 // standard stuff
 #include <iostream>
+#include <cstring>
 
 // toml stuff
 #include <toml.hpp>
+
+// toml helpers
+#define TOML_CSTRING(X) strdup(std::string(X.as_string()).c_str())
+#define TOML_INT(X) X.as_integer()
 
 // json stuff
 #if 0
@@ -19,6 +24,48 @@ using json = nlohmann::json;
 
 #include "toml-parsers.hpp"
 
+static ActorDatabase::Entry::Property ActorDatabasePropertyFromToml(toml::value property)
+{
+	ActorDatabase::Entry::Property result;
+	
+	result.mask = TOML_INT(property["Mask"]);
+	result.name = TOML_CSTRING(property["Name"]);
+	result.target = TOML_CSTRING(property["Target"]);
+	
+	auto dropdown = property["Dropdown"];
+	
+	for (auto &each : dropdown.as_array())
+	{
+		result.AddCombo(TOML_INT(each[0]), TOML_CSTRING(each[1]));
+	}
+	
+	return result;
+}
+
+static std::vector<ActorDatabase::Entry::Property> ActorDatabasePropertiesFromToml(toml::value properties)
+{
+	std::vector<ActorDatabase::Entry::Property> result;
+	
+	for (auto &property : properties.as_array())
+	{
+		result.emplace_back(ActorDatabasePropertyFromToml(property));
+	}
+	
+	return result;
+}
+
+static ActorDatabase::Entry ActorDatabaseEntryFromToml(toml::value tomlActor)
+{
+	ActorDatabase::Entry entry;
+	
+	entry.name = TOML_CSTRING(tomlActor["Name"]);
+	entry.index = TOML_INT(tomlActor["Index"]);
+	entry.object = TOML_INT(tomlActor["Object"]);
+	entry.properties = ActorDatabasePropertiesFromToml(tomlActor["Property"]);
+	
+	return entry;
+}
+
 // testing toml code
 void TomlTest(void)
 {
@@ -27,7 +74,7 @@ void TomlTest(void)
 		Title = "Example"
 		[[Actor]]
 			Name = "Stalfos"
-			Number = 0x0002
+			Index = 0x0002
 			Object = 0x0032
 			BinaryTest = 0b0011
 			
@@ -44,7 +91,7 @@ void TomlTest(void)
 		
 		[[Actor]]
 			Name = "Wolfos"
-			Number = 0x01AF
+			Index = 0x01AF
 			Object = 0x0183
 			BinaryTest = 0b0011
 			
@@ -58,6 +105,67 @@ void TomlTest(void)
 				Name           = "Get Flag"
 				Target         = "XRot"
 				Dropdown       = [ [ 0xFF, "NULL"] ]
+		
+		[[Actor]]
+			Name = "Test"
+			Index = 0x1234
+			Object = 0x5678
+			
+			[[Actor.Property]]
+				Mask          = 0xFF00
+				Name          = "Set Flag"
+				Target        = "XRot"
+				Dropdown = [ [ 0xFF, "NULL"] ]
+			[[Actor.Property]]
+				Mask           = 0x00FF
+				Name           = "Get Flag"
+				Target         = "XRot"
+				Dropdown = [ [ 0xFF, "NULL"] ]
+			[[Actor.Property]]
+				Mask          = 0xC000
+				Name          = "Set Flag Type"
+				Target        = "Var"
+				Dropdown = [
+					[ 0, "Switch"      ],
+					[ 1, "Chest"       ],
+					[ 2, "Collectible" ],
+					[ 3, "Global"      ],
+				]
+			[[Actor.Property]]
+				Mask          = 0x3000
+				Name          = "Get Flag Type"
+				Target        = "Var"
+				Dropdown = [
+					[ 0, "Switch"      ],
+					[ 1, "Chest"       ],
+					[ 2, "Collectible" ],
+					[ 3, "Global"      ],
+				]
+			[[Actor.Property]]
+				Mask          = 0xFF00
+				Name          = "Flip Get"
+				Target        = "YRot"
+				Dropdown = [ [ 0, "false"], [ 1, "true" ] ]
+			[[Actor.Property]]
+				Mask          = 0x0C00
+				Name          = "Force"
+				Target        = "Var"
+				Dropdown = [ [ 0, "false"], [ 1, "true" ] ]
+			[[Actor.Property]]
+				Mask          = 0x03FF
+				Name          = "Distance x10"
+				Target        = "Var"
+				Dropdown = [ [ 0, "TODO blank dropdown" ] ]
+			[[Actor.Property]]
+				Mask          = 0xF0
+				Name          = "Distance Type"
+				Target        = "YRot"
+				Dropdown = [ [ 0, "XZ" ], [ 1, "XZY" ] ]
+			[[Actor.Property]]
+				Mask          = 0xF
+				Name          = "Header Index"
+				Target        = "YRot"
+				Dropdown = [ [ 0, "TODO blank dropdown" ] ]
 	)";
 	std::stringstream test(tomlData);
 	auto data = toml::parse(test);
@@ -71,12 +179,33 @@ void TomlTest(void)
 	//std::cout << data["Property"] << std::endl;
 	//std::cout << data << std::endl;
 	
-	auto actors = data["Actor"];
-	if (actors.is_array()) std::cout << "is array" << std::endl;
-	for (auto &actor : actors.as_array())
+	auto tomlActors = data["Actor"].as_array();
+	
+	ActorDatabase actorDatabase;
+	
+	// load actor database
+	for (auto &tomlActor : tomlActors)
 	{
-		//std::cout << "cool" << std::endl;
-		std::cout << actor["Name"] << std::endl;
+		auto actor = ActorDatabaseEntryFromToml(tomlActor);
+		
+		actorDatabase.AddEntry(actor);
+	}
+	
+	// display actor database
+	for (auto &actor : actorDatabase.entries)
+	{
+		if (!actor.name)
+			continue;
+		
+		fprintf(stderr, "%s : %04x %04x\n", actor.name, actor.index, actor.object);
+		
+		for (auto &prop : actor.properties)
+		{
+			fprintf(stderr, "  %s in %s\n", prop.name, prop.target);
+			
+			for (auto &combo : prop.combos)
+				fprintf(stderr, "     %s = %d\n", combo.label, combo.value);
+		}
 	}
 }
 
--- src/toml-parsers.hpp
+++ src/toml-parsers.hpp
@@ -5,4 +5,63 @@
 // it's relatively slow to compile
 //
 
+#include <stdint.h>
+
+#include <vector>
+
+struct ActorDatabase
+{
+	struct Entry
+	{
+		struct Property
+		{
+			struct Combo
+			{
+				const char   *label;
+				uint16_t      value;
+			};
+			
+			const char          *target;
+			const char          *name;
+			uint16_t             mask;
+			std::vector<Combo>   combos;
+			
+			void AddCombo(uint16_t value, const char *label)
+			{
+				Combo combo = { label, value };
+				
+				combos.emplace_back(combo);
+			}
+		};
+		
+		const char             *name;
+		uint16_t                index;
+		uint16_t                object;
+		std::vector<Property>   properties;
+		
+		void AddProperty(Property property)
+		{
+			properties.emplace_back(property);
+		}
+	};
+	
+	std::vector<Entry>   entries;
+	
+	void AddEntry(Entry entry)
+	{
+		// make sure it will fit
+		if (entry.index >= entries.size())
+			entries.resize(entry.index + 1);
+		
+		entries[entry.index] = entry;
+	}
+};
+
+struct ObjectDatabase
+{
+	struct ObjectDatabaseEntry
+	{
+	};
+};
+
 void TomlTest(void);
