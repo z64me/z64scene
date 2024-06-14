ae69f6ea2a4f2d6c3f72e94d8210f2c891a19271
z64me <45853441+z64me@users.noreply.github.com>
2024-06-14 19:37:07 -0400

    delete old build scripts

--- ready-deps.sh
+++
@@ -1,9 +0,0 @@
-
-mkdir -p bin/o/imgui
-
-# build the objects
-(cd imgui/examples/example_glfw_opengl3 && make && rm main.o)
-
-# move them to where the build script expects them to be
-cp imgui/examples/example_glfw_opengl3/*.o bin/o/imgui
-
