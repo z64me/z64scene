cde85bed163bbb6e408067de3edd1442e77e2d2d
z64me <45853441+z64me@users.noreply.github.com>
2024-06-19 23:29:03 -0400

    testing wrenlang some more

--- src/test.c
+++ src/test.c
@@ -4,10 +4,13 @@
 // where code is sometimes tested before being integrated
 //
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <wren.h>
 
 #include "misc.h"
+
+#define CODE_AS_WRENHOOK(...) "class hooks { static draw() { " # __VA_ARGS__ " } } "
 
 static void writeFn(WrenVM* vm, const char* text)
 {
@@ -42,8 +45,18 @@ void TestWren(void)
 	WrenVM* vm = wrenNewVM(&config);
 	
 	const char* module = "main";
-	const char* script = CODE_AS_STRING(System.print("I am running in a VM!"));
+	const char* script = CODE_AS_STRING(
+		class hooks {
+			static draw() {
+				System.print("ran draw()")
+			}
+		}
+		\n // unfortunately, wren requires newline
+		System.print("ran wrenInterpet()")
+	);
+	script = CODE_AS_WRENHOOK( System.print("ran autowrapped draw() function") );
 	
+	// compile the code
 	WrenInterpretResult result = wrenInterpret(vm, module, script);
 	
 	switch (result)
@@ -59,5 +72,27 @@ void TestWren(void)
 			break;
 	}
 	
-	wrenFreeVM(vm);
+	// success
+	if (result == WREN_RESULT_SUCCESS)
+	{
+		// get handles
+		wrenEnsureSlots(vm, 1);
+		wrenGetVariable(vm, module, "hooks", 0);
+		WrenHandle *class = wrenGetSlotHandle(vm, 0);
+		WrenHandle *handle = wrenMakeCallHandle(vm, "draw()");
+		
+		// use handles
+		for (int i = 0; i < 10; ++i) {
+			wrenSetSlotHandle(vm, 0, class); // is necessary each time
+			if (wrenCall(vm, handle) != WREN_RESULT_SUCCESS)
+				fprintf(stderr, "failed to invoke function\n");
+		}
+		
+		// free handles
+		wrenReleaseHandle(vm, class);
+		wrenReleaseHandle(vm, handle);
+	}
+	fprintf(stderr, "done\n");
+	
+	//exit(0);
 }
